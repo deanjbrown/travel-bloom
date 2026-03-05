40d6dd538e4d13d13bb5e8515400289d8ae73dab
Dean Brown <deanjbrown221@outlook.com>
2026-03-05 21:43:55 +0000

    Initial Commit

---
+++ scripts/travelBloom.js
@@ -0,0 +1,14 @@
+console.log("travelBloom.js loaded. . .");
+
+// Calculate the size of the nav at runtime
+function setNavHeightProp() {
+  const nav = document.querySelector("nav");
+  const navHeight = nav.offsetHeight;
+  document.documentElement.style.setProperty("--nav-height", `${navHeight}px`);
+}
+
+// Call this function once when the script is loaded and then again anytime the page is resized:
+setNavHeightProp();
+document.addEventListener("resize", setNavHeightProp);
+
+
